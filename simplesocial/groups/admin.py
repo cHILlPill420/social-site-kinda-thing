0dbe0c8501d4fe8d12dff336cad63679e5ad3eea
cHILLPill420 <ankitraya.ar@outlook.com>
2021-07-02 15:07:13 +0545

    admin.site.registering models

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from groups import models
 
 # Register your models here.
+
+#since Group is parent class of GroupMember inline class will help to access child class from the same page as parent class or something like that
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
