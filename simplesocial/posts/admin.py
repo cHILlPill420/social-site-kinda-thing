d65eda6eec36dc56678adc42432041d3e6032e4a
cHILLPill420 <ankitraya.ar@outlook.com>
2021-07-13 19:03:44 +0545

    admin panel customization basics

--- simplesocial/posts/admin.py
+++ simplesocial/posts/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from posts import models
 # Register your models here.
-admin.site.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ['user', 'group', 'message']
+    search_fields = ['message','group'] #not for foreignkey therefore for 'group' it wouldn't work
+    list_filter = ['group', 'user']
+    list_display = ['message', 'group', 'user']
+    list_editable = ['user']
+
+admin.site.register(models.Post, PostAdmin)
